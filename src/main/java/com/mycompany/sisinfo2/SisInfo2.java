2c1ea294ebd0ca809eab808067079d2f7c023f22
Joel Luizaga <luizagajoel8@gmail.com>
2026-05-30 18:31:23 -0400

    Primer commit del proyecto SisInfo2

--- src/main/java/com/mycompany/sisinfo2/SisInfo2.java
+++ src/main/java/com/mycompany/sisinfo2/SisInfo2.java
@@ -3,6 +3,6 @@ package com.mycompany.sisinfo2;
 public class SisInfo2 {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("httola mundo");
     }
 }
